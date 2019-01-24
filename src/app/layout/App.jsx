4d3f61cf9f1b7c8257612bbe069d4dbaeaa51a64
Sardorjon Nazirov <sardor.nazirov@gmail.com>
2019-01-24 09:35:33 -0500

    Folder setup

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-
+import EventDashboard from '../../features/event/eventDashboard/EventDashboard.jsx';
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Eventss</h1>
+        <EventDashboard/>
       </div>
     );
   }
